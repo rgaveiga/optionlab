5bc4c9a028dcc1e49618cf9f0872d327f638f01d
Roberto Gomes, PhD <88116667+rgaveiga@users.noreply.github.com>
2023-11-18 19:14:49 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__),fname)).read()
 
 setup(name='optionlab',
-      version='0.1.5',
+      version='0.1.6',
       description="A Python library for evaluating option trading strategies.",
+      long_description_content_type='text/markdown',
+      long_description=read('Desc.MD'),
       author='Roberto Gomes de Aguiar Veiga',
       url="https://github.com/rgaveiga/optionlab",
       packages=['optionlab'])
