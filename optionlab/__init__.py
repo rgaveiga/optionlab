104e4f6ef820c12c9b63b307af6a37411d83ef86
marc-at-brightnight <76454594+marc-at-brightnight@users.noreply.github.com>
2024-03-11 20:01:29 -0400

    Delete setup.py in favor of pyproject.toml (#10)
    - Repository can now be published via poetry
    - Bumped version to 0.2.0

--- optionlab/__init__.py
+++ optionlab/__init__.py
@@ -1,9 +1,4 @@
-from . import __holidays__
-from . import black_scholes
-from . import support
-from . import strategy
-from . import option_chain
 
-VERSION = "0.1.7"
+VERSION = "0.2.0"
 
 __version__ = VERSION
